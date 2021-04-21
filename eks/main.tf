4dce02901a8aa98d64fda82d5b761a3dcaf1bfcb
Firas Taher <ftaher@QSI030964.local>
2021-04-21 09:13:17 -0500

    Creating fargate profile

--- eks/main.tf
+++ eks/main.tf
@@ -90,17 +90,42 @@ resource "aws_security_group" "cluster_sg" {
   }
 }
 
+resource "aws_iam_role" "fargate_pod_execution_role" {
+  name                  = "${var.name}-eks-fargate-pod-execution-role"
+  force_detach_policies = true
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "eks.amazonaws.com",
+          "eks-fargate-pods.amazonaws.com"
+          ]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSFargatePodExecutionRolePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.fargate_pod_execution_role.name
+}
+
 resource "aws_eks_fargate_profile" "main" {
   cluster_name           = aws_eks_cluster.main.name
   fargate_profile_name   = "fp-default"
   pod_execution_role_arn = aws_iam_role.fargate_pod_execution_role.arn
-  subnet_ids             = var.private_subnets.*.id
+  subnet_ids             = var.secondary_subnet_ids
 
   selector {
     namespace = "default"
-  }
-  selector {
-    namespace = "2048-game"
   }
   selector {
     namespace = "kube-system"
@@ -110,9 +135,6 @@ resource "aws_eks_fargate_profile" "main" {
   }
   selector {
     namespace = "alb-ingress"
-  }
-  selector {
-    namespace = "metrics-system"
   }
   selector {
     namespace = "appmesh-gateway"
